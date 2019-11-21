8491339d127b56e773a41bdc57b60753339b488a
Eduardojvr <eduardojvr10@gmail.com>
2019-11-21 11:58:27 -0300

    Atualizando urls

--- visual/urls.py
+++ visual/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('blank', views.blank, name='blank'),
+    path('charts', views.charts, name='charts'),
+    path('forgot', views.forgot, name='forgot'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('tables', views.tables, name='tables'),
+    path('notfound', views.notfound, name='notfound'),
+
 ]
--- visual/views.py
+++ visual/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def notfound(request):
+    return render(request, '404.html')
+
+def blank(request):
+    return render(request, 'blank.html')
+
+def forgot(request):
+    return render(request, 'forgot-password.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def tables(request):
+    return render(request, 'tables.html')
+
+def charts(request):
+    return render(request, 'charts.html')
