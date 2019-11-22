32f01b2bac734f6433652303c69c20725912da58
Eduardojvr <eduardojvr10@gmail.com>
2019-11-22 20:47:29 -0300

    Adicionando primeiros gráficos da dash

--- visual/models.py
+++ visual/models.py
@@ -1,3 +1,45 @@
-from django.db import models
+import mysql.connector
+import simplejson
 
-# Create your models here.
+class Banco:
+    def partido(self):
+        cnx = mysql.connector.connect(user='admin', password='@d2019',
+                                host='127.0.0.1',
+                                database='candidatura')
+        cursor = cnx.cursor()
+        query = "select p.sigla, count(c.idCandidato) from candidato c inner join PARTIDO p on c.codPartido=p.codPartido group by c.codPartido;"
+        
+        cursor.execute(query)
+        lista_dict = []
+
+        for (sigla, qtd) in cursor:
+            lista = {
+                "sigla": sigla,
+                "qtd": qtd
+            }
+            lista_dict.append(lista)
+        cursor.close()
+        cnx.close()
+        return lista_dict
+
+    def raca(query):
+        cnx = mysql.connector.connect(user='admin', password='@d2019',
+                              host='127.0.0.1',
+                              database='candidatura')
+        
+        cursor = cnx.cursor()
+        query = "select descRaca, count(c.idCandidato) from candidato c inner join raca r on r.codRaca=c.codRaca group by r.descRaca;"
+        
+        cursor.execute(query)
+        lista_dict = []
+
+        for (descRaca, qtd) in cursor:
+            lista = {
+                "descRaca": descRaca,
+                "qtd": qtd
+            }
+            lista_dict.append(lista)
+        cursor.close()
+        cnx.close()
+        return lista_dict
+
--- visual/views.py
+++ visual/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
+from visual.models import *
+import simplejson, json
 
 def index(request):
-    return render(request, 'index.html')
-
-def notfound(request):
-    return render(request, '404.html')
-
-def blank(request):
-    return render(request, 'blank.html')
-
-def forgot(request):
-    return render(request, 'forgot-password.html')
-
-def login(request):
-    return render(request, 'login.html')
-
-def register(request):
-    return render(request, 'register.html')
-
-def tables(request):
-    return render(request, 'tables.html')
-
-def charts(request):
-    return render(request, 'charts.html')
+    b = Banco()
+    lista_json_partido = simplejson.dumps(b.partido())
+    lista_json_raca = simplejson.dumps(b.raca())
+    return render(request, 'index.html', {"lista" : lista_json_partido, "lista_json_raca" : lista_json_raca})
